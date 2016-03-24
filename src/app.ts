9157affeafbd18eb935aa90fafd7290e000dd063
Nate West <natescott.west@gmail.com>
2016-03-24 13:46:33 -0700

    Move template directory to a more sane place

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import * as yaml from "js-yaml";
 import * as fs from "fs";
 import * as Promise from "bluebird";
+import * as nunjucks from "nunjucks";
+import { generateNodeContext } from "./bases/node_base";
 
 const pReadFile = Promise.promisify(fs.readFile);
 
 const loadServiceConfig = (fileBuffer: Buffer) => yaml.load(fileBuffer.toString());
 
-const generateContext = (config) => config;
+const generateContext = (config) => generateNodeContext(config);
 
-const renderTemplates = (context) => console.log(context);
+const renderTemplates = (context) =>
+  console.log(nunjucks.render("templates/README.md.njk", context));
 
 pReadFile("service.yml")
   .then(loadServiceConfig)
